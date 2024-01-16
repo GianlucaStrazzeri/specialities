4ea7470ea43f8361b78bb9a20c0093b1719a23ed
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-01-16 09:06:32 +0100

    creadas dependencias desde express entre rutas

---
+++ app.js
@@ -0,0 +1,49 @@
+const express=require("express");
+const app=express();
+
+app.get("/",(req,res)=>{
+res.send(`<h1>Hola esta es la pagina de la home</h1>
+<a href="/marketing">Marketing </a>
+<a href="/developer">Developer </a>
+<a href="/e404"> Al darle click aparecerá un error 404 ya que esta pagina no existe </a>
+`)
+console.log(req.path);
+});
+
+app.get("/marketing",(req,res)=>{
+    res.send(`<h1>Hola esta es la pagina de Marketing</h1>
+    <a href="/"> Home</a>
+    <a href="/developer"> Developer </a>
+    
+    `)
+    console.log(req.path);
+})
+
+app.get("/developer",(req,res)=>{
+    res.send(`<h1>Hola esta es la pagina de developers</h1>
+    <a href="/"> Home</a>
+    <a href="/marketing"> Marketing</a>
+    `)
+    console.log(req.path);
+})
+
+
+app.use((req,res)=>{
+    res.status(404).send(`<h1>No se ha encontrado la pagina seleccionada, por favor
+    vuelve a la home </h1> <a href="/">Home</a>`)
+    });
+
+
+
+app.listen(3000,()=>{
+    console.log("Escuchando en el puerto 3000")
+})
+
+
+
+
+//De momento me permite entrar y circular entre las siguientes paginas además se ha añadido
+// un manejo de error 404
+// http://localhost:3000/ 
+//http:localhost:3000/marketing
+//http:localhost:3000/developer
